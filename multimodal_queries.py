e75ebd0a8b06729c6c03bf299f4bb2a272d1285d
data-george <georgefrancispinto@gmail.com>
2025-12-29 15:35:21 -0500

    encode images

--- multimodal_queries.py
+++ multimodal_queries.py
@@ -20,4 +20,9 @@ url_image_2 = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.clou
 url_image_3 = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/KCh_pM9BVWq_ZdzIBIA9Fw/image-3.png'
 url_image_4 = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/VaaYLw52RaykwrE3jpFv7g/image-4.png'
 
-image_urls = [url_image_1, url_image_2, url_image_3, url_image_4] 
+image_urls = [url_image_1, url_image_2, url_image_3, url_image_4]
+
+encoded_images = []
+
+for url in image_urls: 
+    encoded_images.append(base64.b64encode(requests.get(url).content).decode("utf-8"))
