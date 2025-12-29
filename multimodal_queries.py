de562d545e021a2f81ac1ab4390d444a95b3c914
data-george <georgefrancispinto@gmail.com>
2025-12-29 15:31:33 -0500

    set up credentials object

--- multimodal_queries.py
+++ multimodal_queries.py
@@ -8,3 +8,9 @@ from ibm_watsonx_ai.foundation_models.schema import TextChatParameters
 from ibm_watsonx_ai.metanames import GenTextParamsMetaNames
 
 from PIL import Image
+
+credentials = Credentials(
+                   url = "https://us-south.ml.cloud.ibm.com",
+                   # api_key = "<YOUR_API_KEY>" # Normally you'd put an API key here, but we've got you covered here
+                  )
+client = APIClient(credentials)
