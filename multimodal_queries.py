ba106b00b9e816389c549e954d4814dc945667c6
data-george <georgefrancispinto@gmail.com>
2025-12-29 15:34:00 -0500

    load test images

--- multimodal_queries.py
+++ multimodal_queries.py
@@ -14,3 +14,10 @@ credentials = Credentials(
                    # api_key = "<YOUR_API_KEY>" # Normally you'd put an API key here, but we've got you covered here
                   )
 client = APIClient(credentials)
+
+url_image_1 = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/5uo16pKhdB1f2Vz7H8Utkg/image-1.png'
+url_image_2 = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/fsuegY1q_OxKIxNhf6zeYg/image-2.png'
+url_image_3 = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/KCh_pM9BVWq_ZdzIBIA9Fw/image-3.png'
+url_image_4 = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/VaaYLw52RaykwrE3jpFv7g/image-4.png'
+
+image_urls = [url_image_1, url_image_2, url_image_3, url_image_4] 
