af48b3a05c4e69e5831cf9f4ceba77854b8d186c
data-george <georgefrancispinto@gmail.com>
2025-12-29 15:29:01 -0500

    create multimodal_queries script and add imports

---
+++ multimodal_queries.py
@@ -0,0 +1,10 @@
+import requests
+import base64
+import os
+from ibm_watsonx_ai import Credentials
+from ibm_watsonx_ai import APIClient
+from ibm_watsonx_ai.foundation_models import Model, ModelInference
+from ibm_watsonx_ai.foundation_models.schema import TextChatParameters
+from ibm_watsonx_ai.metanames import GenTextParamsMetaNames
+
+from PIL import Image
