701b24f056aa2f622d9f7b9f1e52960d0870508d
data-george <georgefrancispinto@gmail.com>
2025-12-29 17:46:49 -0500

    complete code for crew script including agents and tasks

--- src/crew.py
+++ src/crew.py
@@ -36,17 +36,141 @@ class BaseNourishBotCrew:
         
         with open(self.tasks_config_path, 'r') as f:
             self.tasks_config = yaml.safe_load(f)
-            
-    ## TODO: Define the agents and tasks for the NourishBot crew
+
+    @agent
+    def ingredient_detection_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['ingredient_detection_agent'],
+            tools=[
+                ExtractIngredientsTool.extract_ingredient, 
+                FilterIngredientsTool.filter_ingredients
+            ],
+            allow_delegation=False,
+            max_iter=5,
+            verbose=True
+        )
+
+    @agent
+    def dietary_filtering_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['dietary_filtering_agent'],
+            tools=[DietaryFilterTool.filter_based_on_restrictions],
+            allow_delegation=True,
+            max_iter=6,
+            verbose=True
+        )
+
+    @agent
+    def nutrient_analysis_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['nutrient_analysis_agent'],
+            tools=[NutrientAnalysisTool.analyze_image],
+            allow_delegation=False,
+            max_iter=4,
+            verbose=True
+        )
+
+    @agent
+    def recipe_suggestion_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['recipe_suggestion_agent'],
+            allow_delegation=False,
+            verbose=True
+        )
+
+    @task
+    def ingredient_detection_task(self) -> Task:
+        task_config = self.tasks_config['ingredient_detection_task']
+
+        return Task(
+            description=task_config['description'],
+            agent=self.ingredient_detection_agent(),
+            expected_output=task_config['expected_output']
+        )
+
+    @task
+    def dietary_filtering_task(self) -> Task:
+        task_config = self.tasks_config['dietary_filtering_task']
+
+        return Task(
+            description=task_config['description'],
+            agent=self.dietary_filtering_agent(),
+            depends_on=['ingredient_detection_task'],
+            input_data=lambda outputs: {
+                'ingredients': outputs['ingredient_detection_task'],
+                'dietary_restrictions': self.dietary_restrictions
+            },
+            expected_output=task_config['expected_output']
+        )
+
+    @task
+    def nutrient_analysis_task(self) -> Task:
+        task_config = self.tasks_config['nutrient_analysis_task']
+
+        return Task(
+            description=task_config['description'],
+            agent=self.nutrient_analysis_agent(),
+            expected_output=task_config['expected_output'],
+            output_json=NutrientAnalysisOutput
+        )
+
+    @task
+    def recipe_suggestion_task(self) -> Task:
+        task_config = self.tasks_config['recipe_suggestion_task']
+
+        return Task(
+            description=task_config['description'],
+            agent=self.recipe_suggestion_agent(),
+            depends_on=['dietary_filtering_task'],
+            input_data=lambda outputs: {
+                'filtered_ingredients': outputs['dietary_filtering_task']
+            },
+            expected_output=task_config['expected_output'],
+            output_json=RecipeSuggestionOutput
+        )
 
 
 @CrewBase
 class NourishBotRecipeCrew(BaseNourishBotCrew):
 
-    ##TODO: Define the agents and tasks for the NourishBotRecipeCrew
+    @crew
+    def crew(self) -> Crew:
+        tasks = [
+            self.ingredient_detection_task(),
+            self.dietary_filtering_task(),
+            self.recipe_suggestion_task()
+        ]
+
+        agents = [
+            self.ingredient_detection_agent(),
+            self.dietary_filtering_agent(),
+            self.recipe_suggestion_agent()
+        ]
+
+        return Crew(
+            agents=agents,
+            tasks=tasks,
+            process=Process.sequential,
+            verbose=True
+        )
 
 
 @CrewBase
 class NourishBotAnalysisCrew(BaseNourishBotCrew):
 
-    ## TODO: Define the agents and tasks for the NourishBotAnalysisCrew
+    @crew
+    def crew(self) -> Crew:
+        tasks = [
+            self.nutrient_analysis_task(),
+        ]
+
+        agents = [
+            self.nutrient_analysis_agent(),
+        ]
+
+        return Crew(
+            agents=agents,
+            tasks=tasks,
+            process=Process.sequential,
+            verbose=True
+        )
